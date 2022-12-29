5d106797ccf57b1cdffd721fe32c3e075ea6403c
syunsuke sasaki <sasaki2931@gmail.com>
2022-12-29 18:26:50 +0900

    タイムゾーン

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
+
 
 
 
@@ -11,7 +10,8 @@ Bundler.require(*Rails.groups)
 
 module Kadai7
   class Application < Rails::Application
-    
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 6.1
   end 
 end
